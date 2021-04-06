1c79ab91c2bfab257a890b6a11930d79966676d5
Berrones Edwin <Berrones.Edwin@hotmail.com>
2021-04-06 13:24:02 -0500

    add react Router- useContext

---
+++ src/components/09.-useContext/AppRouter.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+
+import HomeScreen from "./HomeScreen";
+
+import AboutScreen from "./AboutScreen";
+import LoginScreen from "./LoginScreen";
+import NavBar from "./NavBar";
+
+const AppRouter = () => {
+    return (
+        
+        <Router>
+            <NavBar />
+            <div>
+                <Switch>
+                    <Route exact path="/" component={HomeScreen} />
+                    <Route exact path="/about" component={AboutScreen} />
+                    <Route exact path="/login" component={LoginScreen} />
+                    <Redirect path="/" />
+                </Switch>
+            </div>
+        </Router>
+    );
+};
+
+export default AppRouter;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 // import MemoHook from './components/06.-Memos/MemoHook';
 // import Padre from './components/07.-Tarea/Padre';
-import ToDoApp from './components/08.-useReducer/ToDoApp';
+// import ToDoApp from './components/08.-useReducer/ToDoApp';
+import MainApp from './components/09.-useContext/MainApp';
 // import MultipleCustomHooks from './components/03.-Examples/MultipleCustomHooks';
 // import ExampleReal from './components/04.-USeRef/ExampleReal';
 // import Memorize from './components/06.-Memos/Memorize';
@@ -16,4 +17,4 @@ import "./index.css";
 const div = document.getElementById('root')
 
 // ReactDOM.render( <CounterApp value={7}  /> ,div)
-ReactDOM.render(<ToDoApp /> ,div)
+ReactDOM.render(<MainApp /> ,div)
